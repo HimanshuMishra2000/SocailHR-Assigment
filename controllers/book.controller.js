b1dac1916a04163bae1adee8e330715d359a923f
Himanshu Mishra <93372872+HimanshuMishra2000@users.noreply.github.com>
2022-10-29 12:09:32 +0530

    Update book.controller.js

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -5,51 +5,6 @@ const express = require("express");
 
 const router = express.Router()
 
-
-// router.post("/", async (req, res) => {
-//     try {
-//       const mensProducts = await MensProduct.create(req.body);
-  
-//       res.status(201).send(mensProducts);
-//     } catch (err) {
-//       res.status(500).send(err.message);
-//     }
-//   });
-  
-//   // getting and showing the products to front-end
-  
-//   router.get("/", async (req, res) => {
-//     try {
-//       const mensProducts = await MensProduct.find().lean().exec();
-  
-//       res.status(201).send(mensProducts);
-//     } catch (err) {
-//       res.status(500).send(err.message);
-//     }
-//   });
-  
-//   router.delete("/books/delete/:id", async (req, res) => {
-//     try {
-//       const book = await book.findByIdAndDelete(req.params._id);
-  
-//       res.status(201).send(book);
-//     } catch (err) {
-//       res.status(500).send(err.message);
-//     }
-//   });
-  
-//   router.get("/:_id", async (req, res) => {
-//     //console.log('req.params.id: ', req.params._id);
-//     try {
-//       const mensProduct = await MensProduct.findById(req.params._id)
-//         .lean()
-//         .exec();
-  
-//       res.status(201).send(mensProduct);
-//     } catch (err) {
-//       res.status(500).send(err.message);
-//     }
-//   });
   const postBook = async (req, res) => {
     try {
         let book = await Book.create(req.body);
